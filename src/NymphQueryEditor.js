b3891ceb4ddf83c4df0eb1cb933e2608723fcc85
Hunter Perrin <hperrin@gmail.com>
2018-06-21 16:10:40 -0700

    Added query editor index.

---
+++ src/NymphQueryEditor.js
@@ -0,0 +1,6 @@
+import QueryEditor from './QueryEditor';
+import SelectorEditor from './SelectorEditor';
+import ValueEditor from './ValueEditor';
+
+export default QueryEditor;
+export {QueryEditor, SelectorEditor, ValueEditor};
